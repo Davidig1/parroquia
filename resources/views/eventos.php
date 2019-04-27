bdf11cef8dc698eb16fa431d8dd6d416cd4a3d04
mauriciolimasillero <mauriciolimasillero@users.noreply.github.com>
2019-04-27 11:59:48 -0400

    nueva vista eventos

---
+++ resources/views/eventos.php
@@ -0,0 +1,221 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Santa Maria de los Angeles</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+
+    <!-- Integracion de facebook y twitter -->
+    <meta property="og:title" content=""/>
+    <meta property="og:image" content=""/>
+    <meta property="og:url" content=""/>
+    <meta property="og:site_name" content=""/>
+    <meta property="og:description" content=""/>
+    <meta name="twitter:title" content="" />
+    <meta name="twitter:image" content="" />
+    <meta name="twitter:url" content="" />
+    <meta name="twitter:card" content="" />
+
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville:400,400i,700" rel="stylesheet"
+    >
+    
+    <!-- Animate.css -->
+    <link rel="stylesheet" href="santamaria/css/animate.css">
+    <!-- Icomoon Icon Fonts-->
+    <link rel="stylesheet" href="santamaria/css/icomoon.css">
+    <!-- Bootstrap  -->
+    <link rel="stylesheet" href="santamaria/css/bootstrap.css">
+
+    <!-- Magnific Popup -->
+    <link rel="stylesheet" href="santamaria/css/magnific-popup.css">
+
+    <!-- Flexslider  -->
+    <link rel="stylesheet" href="santamaria/css/flexslider.css">
+
+    <!-- Owl Carousel  -->
+    <link rel="stylesheet" href="santamaria/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="santamaria/css/owl.theme.default.min.css">
+
+    <!-- Theme style  -->
+    <link rel="stylesheet" href="santamaria/css/style.css">
+
+    <!-- Modernizr JS -->
+    <script src="santamaria/js/modernizr-2.6.2.min.js"></script>
+
+    </head>
+    <body>
+        
+    <div class="fh5co-loader"></div>
+    
+    <div id="page">
+    <nav class="fh5co-nav" role="navigation">
+        <div class="container-wrap">
+            <div class="top-menu">
+                <div class="row">
+                    <div class="col-md-12 col-offset-0 text-center">
+                        <div id="fh5co-logo"><a href="welcome">Santa María de los Ángeles</a></div>
+                    </div>
+                    <div class="col-md-12 col-md-offset-0 text-center menu-1">
+                        <ul>
+                            <li><a href="welcome">Inicio</a></li>
+                            <li><a href="sermones">Sermones</a></li>
+                            <li class="has-dropdown">
+                                <a href="">Sacramentos</a>
+                                <ul class="dropdown">
+                                    <li><a href="bautizo">Bautizo</a></li>
+                                    <li><a href="comunion">Primera Comunion</a></li>
+                                    <li><a href="confirmacion">Confirmacion</a></li>
+                                    <li><a href="matrimonio">Matrimonio</a></li>
+                                    <li><a href="catequesis">Catequesis</a></li>
+                                </ul>
+                            </li>
+                            <li class="active"><a href="eventos">Eventos</a></li>
+                            <li><a href="quienessomos">Quienes Somos</a></li>
+                            <li><a href="contactenos">Contactenos</a></li>
+                            <li><a href="{{ route('login') }}">Ingresar</a></li>
+                        </ul>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </nav>
+	<div class="container-wrap">
+		<aside id="fh5co-hero">
+			<div class="flexslider">
+				<ul class="slides">
+			   	<li style="background-image: url(santamaria/images/img_bg_2.jpg);">
+			   		<div class="overlay"></div>
+			   		<div class="container-fluid">
+			   			<div class="row">
+				   			<div class="col-md-6 col-md-offset-3 text-center slider-text">
+				   				<div class="slider-text-inner">
+				   					<h1>Eventos</h1>
+										<h2>Actividades liturgicas mas importantes del año</h2>
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>
+			  	</ul>
+		  	</div>
+		</aside>
+		<div id="fh5co-events">
+			<div class="row animate-box">
+				<div class="col-md-6 col-md-offset-3 text-center fh5co-heading">
+					<h2>Eventos Parroquiales</h2>
+					<p>En esta sección se darán a conocer todas las actividades que se desarrollarán en la parroquia con el objetivo de que la comunidad participe y logremos cada día mantener una iglesia viva.</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-4 animate-box">
+					<div class="events-entry">
+						<span class="date">Del 1 al 24 de Diciembre</span>
+						<h3><a href="adviento">Adviento</a></h3>
+						<p>El sentido del Adviento es avivar en los creyentes la espera del Señor.</p>
+						<a href="calendario">Ver Calendario <i class="icon-arrow-right3"></i></a>
+					</div>
+				</div>
+				<div class="col-md-4 animate-box">
+					<div class="events-entry">
+						<span class="date">25 de Diciembre</span>
+						<h3><a href="navidad">Navidad</a></h3>
+						<p>Fiesta de resonancia universal, el en antes y después del nacimiento de Jesucristo.</p>
+						<a href="calendario">Ver Calendario <i class="icon-arrow-right3"></i></a>
+					</div>
+				</div>
+				<div class="col-md-4 animate-box">
+					<div class="events-entry">
+						<span class="date">Del 6 de Marzo al 18 de Abril</span>
+						<h3><a href="cuaresma">Cuaresma</a></h3>
+						<p>Tiempo litúrgico de conversión, que marca la Iglesia para prepararnos a la gran fiesta de la Pascua.</p>
+						<a href="calendario">Ver Calendario <i class="icon-arrow-right3"></i></a>
+					</div>
+				</div>
+				<div class="col-md-4 animate-box">
+					<div class="events-entry">
+						<span class="date">Del 14 de Abril al 20 de Abril</span>
+						<h3><a href="semanasanta">Semana Santa</a></h3>
+						<p>Conmemoración anual cristiana de la Pasión, Muerte y Resurrección de Jesús de Nazaret.</p>
+						<a href="calendario">Ver Calendario <i class="icon-arrow-right3"></i></a>
+					</div>
+				</div>
+				<div class="col-md-4 animate-box">
+					<div class="events-entry">
+						<span class="date">21 de Abril</span>
+						<h3><a href="pascua">Pascua</a></h3>
+						<p>Resurrección de Jesucristo al tercer día después de haber sido crucificado.</p>
+						<a href="calendario">Ver Calendario <i class="icon-arrow-right3"></i></a>
+					</div>
+				</div>
+				<div class="col-md-4 animate-box">
+					<div class="events-entry">
+						<span class="date">9 de Junio</span>
+						<h3><a href="#">Pentecostes</a></h3>
+						<p>Festividad religiosa que se celebra cincuenta días después de Pascua.</p>
+						<a href="calendario">Ver Calendario <i class="icon-arrow-right3"></i></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div><!-- END container-wrap -->
+
+	<div class="container-wrap">
+        <footer id="fh5co-footer" role="contentinfo">
+            <div class="col-md-4 text-center">
+                <h3>Av. 16 de Julio, El Alto</h3>
+            </div>
+            <div class="col-md-4 text-center">
+                <h2><a href="#">Santa Maria de los Ángeles</a></h2>
+            </div>
+            <div class="col-md-4 text-center">
+                <p>
+                    <ul class="fh5co-social-icons">
+                        <li><a href="#"><i class="icon-twitter2"></i></a></li>
+                        <li><a href="#"><i class="icon-facebook2"></i></a></li>
+                        <li><a href="#"><i class="icon-dribbble2"></i></a></li>
+                    </ul>
+                </p>
+            </div>
+            <div class="row copyright">
+                <div class="col-md-12 text-center">
+                    <p>
+                        <small class="block">&copy; 2019 uni. Todos los derechos Reservados.</small> 
+                        <small class="block">Diseñado por <a href="mauriciolimasillero@gmail.com" target="_blank">mauriciolimasillero@gmail.com</a></small>
+                    </p>
+                </div>
+            </div>
+        </footer>
+    </div><!-- END container-wrap -->
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up2"></i></a>
+	</div>
+	
+	<!-- jQuery -->
+	<script src="santamaria/js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="santamaria/js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="santamaria/js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="santamaria/js/jquery.waypoints.min.js"></script>
+	<!-- Flexslider -->
+	<script src="santamaria/js/jquery.flexslider-min.js"></script>
+	<!-- Carousel -->
+	<script src="santamaria/js/owl.carousel.min.js"></script>
+	<!-- Magnific Popup -->
+	<script src="santamaria/js/jquery.magnific-popup.min.js"></script>
+	<script src="santamaria/js/magnific-popup-options.js"></script>
+	<!-- Counters -->
+	<script src="santamaria/js/jquery.countTo.js"></script>
+	<!-- Main -->
+	<script src="santamaria/js/main.js"></script>
+
+	</body>
+</html>
+
