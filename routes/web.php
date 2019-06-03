94f7d498f5c108fe4bfc469811d74ae271ae093d
Davidig1 <dvisglap@gmail.com>
2019-06-03 15:37:04 -0400

    correccion de error git

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 //Eventos - Dashboard
 Route::get('/home/eventos', 'EventController@index')->name('events.index');
 Route::post('/home/eventos/crear', 'EventController@create')->name('events.create');
-<<<<<<< HEAD
+//<<<<<<< HEAD
 
 
 //Inscripcion sacramentos y requisitos
@@ -41,6 +41,6 @@ Route::post('/home/crearcomunion', 'UserinController@crearcom')->name('comunion.
 Route::post('/home/crearbautismo', 'UserinController@crearbau')->name('bautismo.crear');
 Route::post('/home/crearcatequesis', 'UserinController@crearcat')->name('catequesis.crear');
 Route::post('/home/crearconfirmacion', 'UserinController@crearcon')->name('confirmacion.crear');
-=======
+//=======
 Route::get('/home/eventos/eliminar/{event}', 'EventController@delete')->name('events.delete');
->>>>>>> 9c6382a360ba375fa68f8206d2308b7291741e5f
+//>>>>>>> 9c6382a360ba375fa68f8206d2308b7291741e5f
