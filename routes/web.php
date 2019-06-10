ae60d0b4998a88ab094e9081c3a8faa90c299200
ErickRabpit <ErickRabpit@users.noreply.github.com>
2019-06-09 21:36:47 -0400

    Correccion de rutas 2 (archivo web)

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -9,49 +10,40 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/quienessomos', function () {
-    return view('quienessomos');
-});
-Route::get('/eventos', function () {
-    return view('eventos');
-});
-Route::get('/bautizo', function () {
-    return view('bautizo');
-});
-Route::get('/catequesis', function () {
-    return view('catequesis');
-});
-Route::get('/comunion', function () {
-    return view('comunion');
-});
-Route::get('/confirmacion', function () {
-    return view('confirmacion');
-});
-Route::get('/matrimonio', function () {
-    return view('matrimonio');
-});
-Route::get('/contactenos', function () {
-    return view('contactenos');
-});
-Route::get('/sermones', function () {
-    return view('sermones');
-});
+
+Route::get('/', 'WebController@welcome');
+Route::get('/quienessomos', 'WebController@quienessomos');
+
+
+
+Route::get('/eventos',  'WebController@eventos');
+Route::get('/bautizo',  'WebController@bautizo');
+Route::get('/catequesis', 'WebController@catequesis');
+Route::get('/comunion',  'WebController@comunion');
+Route::get('/confirmacion',  'WebController@confirmacion');
+
+Route::get('/matrimonio',	'WebController@matrimonio');
+Route::get('/contactenos',	'WebController@contactenos');
+Route::get('/sermones',	'WebController@sermones');
+
+
+
 Auth::routes();
+
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+
+
 //Eventos - Dashboard
 Route::get('/home/eventos', 'EventController@index')->name('events.index');
 Route::post('/home/eventos/crear', 'EventController@create')->name('events.create');
-//<<<<<<< HEAD
+Route::get('/home/eventos/eliminar/{event}', 'EventController@delete')->name('events.delete');
+
+
+
 //Inscripcion sacramentos y requisitos
 Route::post('/home/crearcomunion', 'UserinController@crearcom')->name('comunion.crear');
 Route::post('/home/crearbautismo', 'UserinController@crearbau')->name('bautismo.crear');
 Route::post('/home/crearcatequesis', 'UserinController@crearcat')->name('catequesis.crear');
 Route::post('/home/crearconfirmacion', 'UserinController@crearcon')->name('confirmacion.crear');
-//=======
-Route::get('/home/eventos/eliminar/{event}', 'EventController@delete')->name('events.delete');
-//>>>>>>> 9c6382a360ba375fa68f8206d2308b7291741e5f
-
-
