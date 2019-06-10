e921e7ee210d0ed6474ae129e0faefcfb37b949b
ErickRabpit <ErickRabpit@users.noreply.github.com>
2019-06-10 15:34:54 -0400

    Correccion de rutas

---
+++ resources/views/web/contactenos.php
@@ -0,0 +1,187 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Contactenos</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+
+    <!-- Integracion de facebook y twitter -->
+    <meta property="og:title" content=""/>
+    <meta property="og:image" content=""/>
+    <meta property="og:url" content=""/>
+    <meta property="og:site_name" content=""/>
+    <meta property="og:description" content=""/>
+    <meta name="twitter:title" content="" />
+    <meta name="twitter:image" content="" />
+    <meta name="twitter:url" content="" />
+    <meta name="twitter:card" content="" />
+
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville:400,400i,700" rel="stylesheet"
+    >
+    
+    <!-- Animate.css -->
+    <link rel="stylesheet" href="santamaria/css/animate.css">
+    <!-- Icomoon Icon Fonts-->
+    <link rel="stylesheet" href="santamaria/css/icomoon.css">
+    <!-- Bootstrap  -->
+    <link rel="stylesheet" href="santamaria/css/bootstrap.css">
+
+    <!-- Magnific Popup -->
+    <link rel="stylesheet" href="santamaria/css/magnific-popup.css">
+
+    <!-- Flexslider  -->
+    <link rel="stylesheet" href="santamaria/css/flexslider.css">
+
+    <!-- Owl Carousel  -->
+    <link rel="stylesheet" href="santamaria/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="santamaria/css/owl.theme.default.min.css">
+
+    <!-- Theme style  -->
+    <link rel="stylesheet" href="santamaria/css/style.css">
+
+    <!-- Modernizr JS -->
+    <script src="santamaria/js/modernizr-2.6.2.min.js"></script>
+
+    </head>
+	<body>
+		
+	<div class="fh5co-loader"></div>
+	
+	<div id="page">
+	<nav class="fh5co-nav" role="navigation">
+        <div class="container-wrap">
+            <div class="top-menu">
+                <div class="row">
+                    <div class="col-md-12 col-offset-0 text-center">
+                        <div id="fh5co-logo"><a href="">Santa María de los Ángeles</a></div>
+                    </div>
+                    <div class="col-md-12 col-md-offset-0 text-center menu-1">
+                        <ul>
+                            <li><a href="">Inicio</a></li>
+                            <li><a href="sermones">Sermones</a></li>
+                            <li class="has-dropdown">
+                                <a href="">Sacramentos</a>
+                                <ul class="dropdown">
+                                    <li><a href="bautizo">Bautizo</a></li>
+                                    <li><a href="comunion">Primera Comunion</a></li>
+                                    <li><a href="confirmacion">Confirmacion</a></li>
+                                    <li><a href="matrimonio">Matrimonio</a></li>
+                                    <li><a href="catequesis">Catequesis</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="eventos">Eventos</a></li>
+                            <li><a href="quienessomos">Quienes Somos</a></li>
+                            <li class="active"><a href="contactenos">Contactenos</a></li>
+                            <li><a href="login">Ingresar</a></li>
+                        </ul>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </nav>
+	<div class="container-wrap">
+		<aside id="fh5co-hero">
+			<div class="flexslider">
+				<ul class="slides">
+			   	<li style="background-image: url(santamaria/images/img_bg_2.jpg);">
+			   		<div class="overlay"></div>
+			   		<div class="container-fluid">
+			   			<div class="row">
+				   			<div class="col-md-6 col-md-offset-3 text-center slider-text">
+				   				<div class="slider-text-inner">
+				   					<h1>Contactenos</h1>
+										<h2>Mantente al tanto de toda nuestra comunidad <br>contactandote con nosotros</h2>
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>
+			  	</ul>
+		  	</div>
+		</aside>
+		<div id="fh5co-contact">
+			<div class="row animate-box">
+				<div class="col-md-6 col-md-offset-3 text-center fh5co-heading">
+					<h2>Contactenos</h2>
+					<p>Contáctenos aquí y estaremos encantados de responder sus consultas Generales.</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-3 col-md-push-8 animate-box">
+					<h3>Nustra Ubicacion:</h3>
+					<ul class="contact-info">
+						<li><i class="icon-location4"></i>Av. 16 de Julio, El Alto</li>
+						<li><i class="icon-phone3"></i>+59174029878</li>
+						<li><i class="icon-location3"></i><a href="#">santamaria@gmail.com</a></li>
+						<li><i class="icon-globe2"></i><a href="#">www.santamaria.com</a></li>
+					</ul>
+				</div>
+				<div class="col-md-7 col-md-pull-2 animate-box">
+					<div class="row">
+						<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15302.528906848449!2d-68.1772001!3d-16.4941498!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x458d24d4cbfde3fc!2sIglesia+Santa+Maria+de+los+Angeles!5e0!3m2!1ses-419!2sbo!4v1560101761577!5m2!1ses-419!2sbo" width="580" height="200" frameborder="0" style="border:0" allowfullscreen></iframe>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+	</div><!-- END container-wrap -->
+
+	<div class="container-wrap">
+        <footer id="fh5co-footer" role="contentinfo">
+            <div class="col-md-4 text-center">
+                <h3>Av. 16 de Julio, El Alto</h3>
+            </div>
+            <div class="col-md-4 text-center">
+                <h2><a href="#">Santa Maria de los Ángeles</a></h2>
+            </div>
+            <div class="col-md-4 text-center">
+                <p>
+                    <ul class="fh5co-social-icons">
+                        <li><a href="#"><i class="icon-twitter2"></i></a></li>
+                        <li><a href="#"><i class="icon-facebook2"></i></a></li>
+                        <li><a href="#"><i class="icon-dribbble2"></i></a></li>
+                    </ul>
+                </p>
+            </div>
+            <div class="row copyright">
+                <div class="col-md-12 text-center">
+                    <p>
+                        <small class="block">&copy; 2019 uni. Todos los derechos Reservados.</small> 
+                        <small class="block">Diseñado por <a href="mauriciolimasillero@gmail.com" target="_blank">mauriciolimasillero@gmail.com</a></small>
+                    </p>
+                </div>
+            </div>
+        </footer>
+    </div><!-- END container-wrap -->
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up2"></i></a>
+	</div>
+	
+	<!-- jQuery -->
+    <script src="santamaria/js/jquery.min.js"></script>
+    <!-- jQuery Easing -->
+    <script src="santamaria/js/jquery.easing.1.3.js"></script>
+    <!-- Bootstrap -->
+    <script src="santamaria/js/bootstrap.min.js"></script>
+    <!-- Waypoints -->
+    <script src="santamaria/js/jquery.waypoints.min.js"></script>
+    <!-- Flexslider -->
+    <script src="santamaria/js/jquery.flexslider-min.js"></script>
+    <!-- Carousel -->
+    <script src="santamaria/js/owl.carousel.min.js"></script>
+    <!-- Magnific Popup -->
+    <script src="santamaria/js/jquery.magnific-popup.min.js"></script>
+    <script src="santamaria/js/magnific-popup-options.js"></script>
+    <!-- Counters -->
+    <script src="santamaria/js/jquery.countTo.js"></script>
+    <!-- Main -->
+    <script src="santamaria/js/main.js"></script>
+
+	</body>
+</html>
